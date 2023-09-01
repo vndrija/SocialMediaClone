90554d4811551eb6297c007b3f9a8585626229b5
vndrija <matijasevicicon@gmail.com>
2023-09-01 18:04:01 +0200

    made angular project

--- v7-backend/src/main/java/rs/ac/uns/ftn/svtvezbe07/controller/UserController.java
+++ v7-backend/src/main/java/rs/ac/uns/ftn/svtvezbe07/controller/UserController.java
@@ -10,14 +10,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import rs.ac.uns.ftn.svtvezbe07.model.dto.JwtAuthenticationRequest;
 import rs.ac.uns.ftn.svtvezbe07.model.dto.UserDTO;
+import rs.ac.uns.ftn.svtvezbe07.model.entity.Role;
+import rs.ac.uns.ftn.svtvezbe07.model.entity.User;
 import rs.ac.uns.ftn.svtvezbe07.security.TokenUtils;
 import rs.ac.uns.ftn.svtvezbe07.service.UserService;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
 import java.security.Principal;
 import java.util.List;
 
@@ -26,72 +30,30 @@ import java.util.List;
 @RequestMapping("api/users")
 public class UserController {
 
-    @Autowired
-    UserService userService;
 
     @Autowired
-    UserDetailsService userDetailsService;
-
+    UserService service;
     @Autowired
-    AuthenticationManager authenticationManager;
+    private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    TokenUtils tokenUtils;
+    @Transactional
+    @PutMapping("/updatepassword/{old}")
+    public ResponseEntity<User> create(@RequestBody User newUser, @PathVariable("old") String oldPassword) {
 
-    /* Ili preporucen nacin: Constructor Dependency Injection
-    @Autowired
-    public UserController(UserServiceImpl userService, AuthenticationManager authenticationManager,
-                          UserDetailsService userDetailsService, TokenUtils tokenUtils){
-        this.userService = userService;
-        this.authenticationManager = authenticationManager;
-        this.userDetailsService = userDetailsService;
-        this.tokenUtils = tokenUtils;
-    }
-    */
-    @PostMapping("/signup")
-    public ResponseEntity<UserDTO> create(@RequestBody @Validated UserDTO newUser){
+        String encodedPassword = service.geOne(newUser.getUserId()).getPassword();
 
-        User createdUser = userService.createUser(newUser);
-
-        if(createdUser == null){
-            return new ResponseEntity<>(null, HttpStatus.NOT_ACCEPTABLE);
+        boolean match = passwordEncoder.matches(oldPassword, encodedPassword);
+        System.out.println(oldPassword);
+        System.out.println(service.geOne(newUser.getUserId()).getPassword());
+        if(match){
+            newUser.setRole(Role.USER);
+            newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+            User updatedUser = service.save(newUser);
+            return new ResponseEntity<>(updatedUser, HttpStatus.CREATED);
+        }else{
+            return new ResponseEntity<>(newUser, HttpStatus.NOT_ACCEPTABLE);
         }
-        UserDTO userDTO = new UserDTO(createdUser);
 
-        return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<UserTokenState> createAuthenticationToken(
-            @RequestBody JwtAuthenticationRequest authenticationRequest, HttpServletResponse response) {
-
-        // Ukoliko kredencijali nisu ispravni, logovanje nece biti uspesno, desice se
-        // AuthenticationException
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                authenticationRequest.getUsername(), authenticationRequest.getPassword()));
-
-        // Ukoliko je autentifikacija uspesna, ubaci korisnika u trenutni security
-        // kontekst
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        // Kreiraj token za tog korisnika
-        UserDetails user = (UserDetails) authentication.getPrincipal();
-        String jwt = tokenUtils.generateToken(user);
-        int expiresIn = tokenUtils.getExpiredIn();
-
-        // Vrati token kao odgovor na uspesnu autentifikaciju
-        return ResponseEntity.ok(new UserTokenState(jwt, expiresIn));
-    }
-
-    @GetMapping("/all")
-    @PreAuthorize("hasRole('ADMIN')")
-    public List<User> loadAll() {
-        return this.userService.findAll();
-    }
-
-    @GetMapping("/whoami")
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public User user(Principal user) {
-        return this.userService.findByUsername(user.getName());
     }
 }
+
